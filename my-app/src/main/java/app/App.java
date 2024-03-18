6744b6efce1da36d04fa15b2d2ed2e5b34072ff1
azevedo <azevedoantoniocicero@gmail.com>
2024-03-18 11:45:48 -0300

    setup inicial maven

---
+++ my-app/src/main/java/app/App.java
@@ -0,0 +1,14 @@
+package app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String name = "Antonio";
+        System.out.println(name);
+    }
+}
